091088113bfb7a74437c10267044da247c40e5b7
Steven Curtis <stevenpcurtis@hotmail.com>
2023-11-05 11:24:45 +0000

    rename to basic request

--- Sources/NetworkClient/Models/SimpleRequest.swift
+++
@@ -1,9 +0,0 @@
-import Foundation
-
-struct SimpleRequest<T: Decodable>: APIRequest {
-    typealias ResponseDataType = T
-    func parseResponse(data: Data) throws -> ResponseDataType {
-        let decoder = JSONDecoder()
-        return try decoder.decode(ResponseDataType.self, from: data)
-    }
-}
