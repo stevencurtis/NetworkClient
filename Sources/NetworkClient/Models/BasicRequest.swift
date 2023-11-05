10eb717315c5d1ec325467eb2cde08f610c055aa
Steven Curtis <stevenpcurtis@hotmail.com>
2023-11-05 11:27:09 +0000

    expose initializer

--- Sources/NetworkClient/Models/BasicRequest.swift
+++ Sources/NetworkClient/Models/BasicRequest.swift
@@ -2,6 +2,7 @@ import Foundation
 
 public struct BasicRequest<T: Decodable>: APIRequest {
     public typealias ResponseDataType = T
+    public init() { }
     public func parseResponse(data: Data) throws -> ResponseDataType {
         let decoder = JSONDecoder()
         return try decoder.decode(ResponseDataType.self, from: data)
